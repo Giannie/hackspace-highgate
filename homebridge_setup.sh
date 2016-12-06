6b1ea7bdeab95c009d8131a2310bf7ce6ab17422
Giannie <ggrasso@blueyonder.co.uk>
2016-12-06 09:09:58 +0000

    Create homebridge_setup.sh

---
+++ homebridge_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd /tmp
+wget https://nodejs.org/dist/v4.0.0/node-v4.0.0-linux-armv6l.tar.gz 
+tar -xvf node-v4.0.0-linux-armv6l.tar.gz 
+cd node-v4.0.0-linux-armv6l
+sudo cp -R * /usr/local/
+sudo npm install -g --unsafe-perm homebridge hap-nodejs node-gyp
+cd /usr/local/lib/node_modules/homebridge/
+sudo npm install --unsafe-perm bignum
+cd /usr/local/lib/node_modules/hap-nodejs/node_modules/mdns
+sudo node-gyp BUILDTYPE=Release rebuild
